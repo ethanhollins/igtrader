4baab5bd33368cb185cbf695db8b9e2dd94b694c
Ethan <ethanjohol@gmail.com>
2019-12-18 13:39:41 +1100

    testing

--- Chart.py
+++ Chart.py
@@ -100,18 +100,17 @@ class Chart(object):
 			raise Exception("({}) Couldn't retrieve data.".format(self.root.idx))
 
 		print('1')
-		sorted_bids = sorted(result['bids'].items(), key=lambda kv: kv[0])
-		sorted_asks = sorted(result['asks'].items(), key=lambda kv: kv[0])
+		latest_ts = sorted(result['bids'].items(), key=lambda kv: kv[0])[-1][0]
 		print('2')
 
 		if self.root.isWeekend():
 			self.c_bid = []
 			self.c_ask = []
 		else:
-			self.c_bid = sorted_bids[-1][1]
-			self.c_ask = sorted_asks[-1][1]
-			del sorted_bids[-1]
-			del sorted_asks[-1]
+			self.c_bid = result['bids'][latest_ts]
+			self.c_ask = result['asks'][latest_ts]
+			del result['bids'][latest_ts]
+			del result['asks'][latest_ts]
 
 		self.bids_ohlc = np.around(self.bids_ohlc, decimals=5)
 		self.asks_ohlc = np.around(self.asks_ohlc, decimals=5)
